974ae31b9dd6d84cc329ade1100d45ffa7850f6c
xji3 <xji3@ncsu.edu>
2017-02-06 21:22:01 -0500

    forgot to update branch length in 1d and 2d search

--- Code/Run.py
+++ Code/Run.py
@@ -44,6 +44,8 @@ def main(args):
     test = PSJSGeneconv(alignment_file, gene_to_orlg_file, seq_index_file, tree_newick, DupLosList,x_js, pm_model, IGC_pm,
                       node_to_pos, terminal_node_list, save_file)
 
+    x = np.concatenate((x_js, test_JS.x[len(test_JS.jsmodel.x_js):]))
+    test.unpack_x(x)
     if args.dim == 1:
         test.optimize_x_IGC(dimension = 1)
     elif args.dim == 2:
@@ -65,10 +67,57 @@ if __name__ == '__main__':
   
 
 ##    MyStruct = namedtuple('MyStruct', 'paralog1 paralog2 tract_length dim')
-##    args = MyStruct(paralog1 = pair[0], paralog2 = pair[1], tract_length = 30.0, dim = 1)
-
-
-
+##    args = MyStruct(paralog1 = 'YLR406C', paralog2 = 'YDL075W', tract_length = 30.0, dim = 1)
+##
+##
+##    paralog = [args.paralog1, args.paralog2]
+##    
+##    gene_to_orlg_file = '../GeneToOrlg/' + '_'.join(paralog) +'_GeneToOrlg.txt'
+##    alignment_file = '../MafftAlignment/' + '_'.join(paralog) +'/' + '_'.join(paralog) +'_input.fasta'
+##
+##    tree_newick = '../YeastTree.newick'
+##    DupLosList = '../YeastTestDupLost.txt'
+##    terminal_node_list = ['kluyveri', 'castellii', 'bayanus', 'kudriavzevii', 'mikatae', 'paradoxus', 'cerevisiae']
+##    node_to_pos = {'D1':0}
+##    pm_model = 'HKY'
+##    x_js = np.log([ 0.5, 0.5, 0.5,  4.35588244,   0.3])
+##    IGC_pm = 'One rate'
+##
+##    save_file = './save/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_save.txt'
+##    summary_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_summary.txt'
+##
+##    test_JS = JSGeneconv(alignment_file, gene_to_orlg_file, tree_newick, DupLosList,x_js, pm_model, IGC_pm,
+##                      node_to_pos, terminal_node_list, save_file)
+##    test_JS.get_mle()
+##    test_JS.get_expectedNumGeneconv()
+##    test_JS.get_individual_summary(summary_file)
+##
+##    if args.dim == 1:     
+##        save_file = './save/PSJS_dim_1_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_init_' + str(args.tract_length) + '_nonclock_save.txt'
+##        summary_file = './summary/PSJS_dim_1_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_init_' + str(args.tract_length) + '_nonclock_summary.txt'
+##    elif args.dim == 2:
+##        save_file = './save/PSJS_dim_2_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_init_' + str(args.tract_length) + '_nonclock_save.txt'
+##        summary_file = './summary/PSJS_dim_2_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_init_' + str(args.tract_length) + '_nonclock_summary.txt'
+##    else:
+##        save_file = './save/PSJS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_init_' + str(args.tract_length) + '_nonclock_save.txt'
+##        summary_file = './summary/PSJS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_init_' + str(args.tract_length) + '_nonclock_summary.txt'
+##        
+##    seq_index_file = '../MafftAlignment/' + '_'.join(paralog) +'/' + '_'.join(paralog) +'_seq_index.txt'
+##
+##    x_js = np.concatenate((test_JS.jsmodel.x_js[:-1], \
+##                           [ test_JS.jsmodel.x_js[-1] - np.log(args.tract_length), - np.log(args.tract_length) ] ))
+##    test = PSJSGeneconv(alignment_file, gene_to_orlg_file, seq_index_file, tree_newick, DupLosList,x_js, pm_model, IGC_pm,
+##                      node_to_pos, terminal_node_list, save_file)
+##
+##    x = np.concatenate((x_js, test_JS.x[len(test_JS.jsmodel.x_js):]))
+##    test.unpack_x(x)
+##    if args.dim == 1:
+##        test.optimize_x_IGC(dimension = 1)
+##    elif args.dim == 2:
+##        test.optimize_x_IGC(dimension = 2)
+##    else:
+##        test.get_mle()
+##    test.get_individual_summary(summary_file)
 
 
 ##    pairs = []
